d668e31f24ed2668907eddb55d57d837dc09e23c
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-17 15:33:13 +0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -22,6 +22,21 @@ IMG_WIDTH = 2048
 IMG_HEIGHT = IMG_WIDTH // 4
 MODEL_SCALE = 8
 
+def str2coords(s, names=['id', 'yaw', 'pitch', 'roll', 'x', 'y', 'z']):
+    '''
+    Input:
+        s: PredictionString (e.g. from train dataframe)
+        names: array of what to extract from the string
+    Output:
+        list of dicts with keys from `names`
+    '''
+    coords = []
+    for l in np.array(s.split()).reshape([-1, 7]):
+        coords.append(dict(zip(names, l.astype('float'))))
+        if 'id' in coords[-1]:
+            coords[-1]['id'] = int(coords[-1]['id'])
+    return coords
+
 def _regr_preprocess(regr_dict):
     for name in ['x', 'y', 'z']:
         regr_dict[name] = regr_dict[name] / 100
