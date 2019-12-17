476be375dc0340daa7094f58c19b3b9808e61a82
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-17 16:37:33 +0800

    Update and rename EfficientNet-b0+U-net+masks+depth_images.py to Model4.py

---
+++ Model4.py
@@ -0,0 +1,19 @@
+use_depth = True
+from helpers import *
+from efficientnet_pytorch import EfficientNet
+
+checkpoint = 'Model4.pth'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = MyUNet(8).to(device)
+model.load_state_dict(torch.load(checkpoint))
+
+def inference(image, depth):
+  # the inference function of this model
+  
+  img = torch.tensor(image[None]).to(device)
+  depth = torch.tensor(depth[None]).to(device)
+  output = model(img, depth).data.cpu().numpy()
+  coords_pred = extract_coords(output[0])
+  
+  return coords_pred
