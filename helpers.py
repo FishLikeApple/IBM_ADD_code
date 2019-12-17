0549a79c4aaabd4a134bd524eed19ea7b1e826c2
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-17 14:57:13 +0800

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,123 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import cv2
+from tqdm import tqdm_notebook as tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from functools import reduce
+import os
+from sklearn.model_selection import train_test_split
+from scipy.optimize import minimize
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+from torchvision import transforms, utils
+
+IMG_WIDTH = 2048
+IMG_HEIGHT = IMG_WIDTH // 4
+MODEL_SCALE = 8
+
+def _regr_preprocess(regr_dict):
+    for name in ['x', 'y', 'z']:
+        regr_dict[name] = regr_dict[name] / 100
+    regr_dict['roll'] = rotate(regr_dict['roll'], np.pi)
+    regr_dict['pitch_sin'] = sin(regr_dict['pitch'])
+    regr_dict['pitch_cos'] = cos(regr_dict['pitch'])
+    regr_dict.pop('pitch')
+    regr_dict.pop('id')
+    return regr_dict
+
+def _regr_back(regr_dict):
+    for name in ['x', 'y', 'z']:
+        regr_dict[name] = regr_dict[name] * 100
+    regr_dict['roll'] = rotate(regr_dict['roll'], -np.pi)
+    
+    pitch_sin = regr_dict['pitch_sin'] / np.sqrt(regr_dict['pitch_sin']**2 + regr_dict['pitch_cos']**2)
+    pitch_cos = regr_dict['pitch_cos'] / np.sqrt(regr_dict['pitch_sin']**2 + regr_dict['pitch_cos']**2)
+    regr_dict['pitch'] = np.arccos(pitch_cos) * np.sign(pitch_sin)
+    return regr_dict
+
+def preprocess_image(img, is_depth=False):
+    if not is_depth:
+        img = img[img.shape[0] // 2:]
+    bg = np.ones_like(img) * img.mean(1, keepdims=True).astype(img.dtype)
+    bg = bg[:, :img.shape[1] // 4]
+    img = np.concatenate([bg, img, bg], 1)
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    return (img / 255).astype('float32')
+    
+def get_mask_and_regr(img, labels):
+    mask = np.zeros([IMG_HEIGHT // MODEL_SCALE, IMG_WIDTH // MODEL_SCALE], dtype='float32')
+    regr_names = ['x', 'y', 'z', 'yaw', 'pitch', 'roll']
+    regr = np.zeros([IMG_HEIGHT // MODEL_SCALE, IMG_WIDTH // MODEL_SCALE, 7], dtype='float32')
+    coords = str2coords(labels)
+    xs, ys = get_img_coords(labels)
+    for x, y, regr_dict in zip(xs, ys, coords):
+        x, y = y, x
+        x = (x - img.shape[0] // 2) * IMG_HEIGHT / (img.shape[0] // 2) / MODEL_SCALE
+        x = np.round(x).astype('int')
+        y = (y + img.shape[1] // 4) * IMG_WIDTH / (img.shape[1] * 1.5) / MODEL_SCALE
+        y = np.round(y).astype('int')
+        if x >= 0 and x < IMG_HEIGHT // MODEL_SCALE and y >= 0 and y < IMG_WIDTH // MODEL_SCALE:
+            mask[x, y] = 1
+            regr_dict = _regr_preprocess(regr_dict)
+            regr[x, y] = [regr_dict[n] for n in sorted(regr_dict)]
+    return mask, regr
+    
+DISTANCE_THRESH_CLEAR = 2
+
+def convert_3d_to_2d(x, y, z, fx = 2304.5479, fy = 2305.8757, cx = 1686.2379, cy = 1354.9849):
+    # stolen from https://www.kaggle.com/theshockwaverider/eda-visualization-baseline
+    return x * fx / z + cx, y * fy / z + cy
+
+def optimize_xy(r, c, x0, y0, z0):
+    def distance_fn(xyz):
+        x, y, z = xyz
+        x, y = convert_3d_to_2d(x, y, z0)
+        y, x = x, y
+        x = (x - IMG_SHAPE[0] // 2) * IMG_HEIGHT / (IMG_SHAPE[0] // 2) / MODEL_SCALE
+        x = np.round(x).astype('int')
+        y = (y + IMG_SHAPE[1] // 4) * IMG_WIDTH / (IMG_SHAPE[1] * 1.5) / MODEL_SCALE
+        y = np.round(y).astype('int')
+        return (x-r)**2 + (y-c)**2
+    
+    res = minimize(distance_fn, [x0, y0, z0], method='Powell')
+    x_new, y_new, z_new = res.x
+    return x_new, y_new, z0
+
+def clear_duplicates(coords):
+    for c1 in coords:
+        xyz1 = np.array([c1['x'], c1['y'], c1['z']])
+        for c2 in coords:
+            xyz2 = np.array([c2['x'], c2['y'], c2['z']])
+            distance = np.sqrt(((xyz1 - xyz2)**2).sum())
+            if distance < DISTANCE_THRESH_CLEAR:
+                if c1['confidence'] < c2['confidence']:
+                    c1['confidence'] = -1
+    return [c for c in coords if c['confidence'] > 0]
+
+def extract_coords(prediction):
+    logits = prediction[0]
+    regr_output = prediction[1:]
+    points = np.argwhere(logits > logits_threshold)
+    col_names = sorted(['x', 'y', 'z', 'yaw', 'pitch_sin', 'pitch_cos', 'roll'])
+    coords = []
+    for r, c in points:
+        regr_dict = dict(zip(col_names, regr_output[:, r, c]))
+        coords.append(_regr_back(regr_dict))
+        coords[-1]['confidence'] = 1 / (1 + np.exp(-logits[r, c]))
+        coords[-1]['x'], coords[-1]['y'], coords[-1]['z'] = optimize_xy(r, c, coords[-1]['x'], coords[-1]['y'], coords[-1]['z'])
+    coords = clear_duplicates(coords)
+    return coords
+
+def coords2str(coords, names=['yaw', 'pitch', 'roll', 'x', 'y', 'z', 'confidence']):
+    s = []
+    for c in coords:
+        for n in names:
+            s.append(str(c.get(n, 0)))
+    return ' '.join(s)
