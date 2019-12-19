5a70fa295edea8e63cf5b0408337e001a1804c9a
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-19 19:08:45 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -1,4 +1,5 @@
 from helpers import *
+import Model1, Model2, Model3, Model4
 import requests 
 
 # download and unzip models
@@ -14,4 +15,14 @@ with open("models-of-ibm-add.zip", "wb") as code:
       code.write(r.content)
 unzip_single('models-of-ibm-add.zip', 'models/')
 
+# get input of all the models
+image, depth = get_image_and_depth('test_sample.jpg', 'monodepth2/')
 
+# get predictions of all the models
+model1_prediction = Model1.inference(image)
+model2_prediction = Model2.inference(image, depth)
+model3_prediction = Model3.inference(image)
+model4_prediction = Model4.inference(image, depth)
+
+# merge all the predictions
+#prediction = 
