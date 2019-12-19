f24909fa2180b1f88274d12c584b22dedbe78690
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-19 20:07:21 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -2,7 +2,7 @@ from helpers import *
 import requests 
 
 # download, unzip and load models
-header = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 78.0.3904.108 Safari/537.36"
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 78.0.3904.108 Safari/537.36'}
 url = 'https://www.kaggle.com/dataset/965835a48e203aa402d44dc3278357247749ba78497bdb7fd06dfa72ec9d3a98/download' 
 r = requests.get(url, headers=header) 
 with open("models-of-ibm-add.zip", "wb") as code:
