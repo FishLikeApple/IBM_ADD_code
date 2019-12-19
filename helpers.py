e33d2c0e4432dfc106df6dd79e56d63b9fc45aee
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-19 17:23:45 +0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -153,19 +153,16 @@ def get_image_and_depth(input_path, monodepth_path):
 
     return img, depth
 
-def zipDir(dirpath,outFullName):
-    """
-    压缩指定文件夹
-    :param dirpath: 目标文件夹路径
-    :param outFullName: 压缩文件保存路径+xxxx.zip
-    :return: 无
-    """
+import sys, os, zipfile
 
-    zip = zipfile.ZipFile(outFullName, "w", zipfile.ZIP_DEFLATED)
-    for path,dirnames,filenames in os.walk(dirpath):
-        # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
-        fpath = path.replace(dirpath,'')
-
-        for filename in filenames:
-            zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
-    zip.close()
+def unzip_single(src_file, dest_dir, password=False):
+    ''' 解压单个文件到目标文件夹。
+    '''
+    if password:
+        password = password.encode()
+    zf = zipfile.ZipFile(src_file)
+    try:
+        zf.extractall(path=dest_dir, pwd=password)
+    except RuntimeError as e:
+        print(e)
+    zf.close()
