6a7f942b3979c1176a974b517a37a1a1c2bb5a15
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-19 20:36:10 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -4,7 +4,7 @@ import requests
 # download, unzip and load models
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 78.0.3904.108 Safari/537.36'}
 url = 'https://www.kaggle.com/dataset/965835a48e203aa402d44dc3278357247749ba78497bdb7fd06dfa72ec9d3a98/download'
-r = requests.post(url=url, allow_redirects=False)
+r = requests.post(url=url, headers=header, allow_redirects=False)
 print(r.headers)
 r = requests.get(r.headers['location']) 
 with open("models-of-ibm-add.zip", "wb") as code:
