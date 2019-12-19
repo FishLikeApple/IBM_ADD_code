c4e66c6f628843dc5a7cb4c7283bf876cdfc484b
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-19 17:15:07 +0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -152,3 +152,20 @@ def get_image_and_depth(input_path, monodepth_path):
     depth = np.rollaxis(depth, 2, 0)
 
     return img, depth
+
+def zipDir(dirpath,outFullName):
+    """
+    压缩指定文件夹
+    :param dirpath: 目标文件夹路径
+    :param outFullName: 压缩文件保存路径+xxxx.zip
+    :return: 无
+    """
+
+    zip = zipfile.ZipFile(outFullName, "w", zipfile.ZIP_DEFLATED)
+    for path,dirnames,filenames in os.walk(dirpath):
+        # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
+        fpath = path.replace(dirpath,'')
+
+        for filename in filenames:
+            zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
+    zip.close()
