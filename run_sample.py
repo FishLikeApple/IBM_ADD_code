23116e4a1fc530ccb93a04a02f9d2a5f206cf402
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-20 20:17:02 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -44,7 +44,7 @@ def run():
     prediction2_4 = output_merging(model2_prediction, model4_prediction)
     final_prediction = output_merging(prediction1_3, prediction2_4)
 
-    print(final_prediction)
+    cv2.imwrite(args.output, visualize(image, final_prediction))
 
 if __name__ == '__main__':
     run()
