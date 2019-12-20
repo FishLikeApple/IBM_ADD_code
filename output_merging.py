33818c1f6ec44bf3239f1104c9363e6d343452a9
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-20 16:38:30 +0800

    Update output_merging.py

--- output_merging.py
+++ output_merging.py
@@ -29,3 +29,38 @@ else:
             return False, None, None
 
         return True, distance, None
+    
+# the main function
+def output_merging(A_coords, M_coords):
+    
+    new_M_coords = M_coords.copy()
+    for A_coord in A_coords:
+        candidate = {'candidate':None, 'TD':99999999}
+        for j in range(len(M_coords)):
+            M_coord = M_coords[j]
+            enough, TD, RD = is_close_enough(A_coord, M_coord)
+            #print(enough)
+            if enough and M_coord not in paired_M_coords and candidate['TD']>TD:
+                candidate['candidate'] = M_coord
+                candidate['TD'] = TD
+                candidate['index'] = j
+        if candidate['candidate'] == None:
+            new_M_coords.append(A_coord)
+        else:
+            M_coord = candidate['candidate']
+            paired_M_coords.append(M_coord)
+            if merging_plan == 'weighted-add':
+                new_coord = {}
+                for item in ['pitch','yaw','roll','x','y','z']:
+                    total_score = A_coord['confidence'] + M_coord['confidence']
+                    new_coord[item] = (A_coord[item]*(A_coord['confidence']/total_score))\
+                                    + (M_coord[item]*(M_coord['confidence']/total_score))       
+            else:
+                if A_coord['confidence'] > M_coord['confidence']:
+                    new_coord = A_coord
+                else:
+                    new_coord = M_coord
+            new_coord['confidence'] = 1 - ((1-A_coord['confidence'])*(1-M_coord['confidence']))
+            new_M_coords[candidate['index']] = new_coord
+            
+    return new_M_coords
