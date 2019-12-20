e5663ea8de43902ab8eb69eacd8edcc1cf29d955
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-20 16:47:01 +0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -21,6 +21,12 @@ IMG_HEIGHT = IMG_WIDTH // 4
 MODEL_SCALE = 8
 model_directory = 'models/'
 
+def imread(path, fast_mode=False):
+    img = cv2.imread(path)
+    if not fast_mode and img is not None and len(img.shape) == 3:
+        img = np.array(img[:, :, ::-1])
+    return img
+
 def str2coords(s, names=['id', 'yaw', 'pitch', 'roll', 'x', 'y', 'z']):
     '''
     Input:
