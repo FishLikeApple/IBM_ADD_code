5ce42152e461e2a0382efcd01d8f8ee2f04b7fa6
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-20 14:49:14 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -8,8 +8,8 @@ header = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 
 url = 'https://drive.google.com/uc?export=download&confirm=pvHZ&id=1tXez97ZG4ElFTRGVS8ZbTFv8nCtMllqQ'
 r = requests.get(url=url, headers=header, allow_redirects=False)
 soup = BeautifulSoup(r.text, "lxml")
-print(soup.select('#uc-download-link'))
-r = requests.get(soup.select('#uc-download-link')[0].href) 
+print(soup.select('#uc-download-link')[0])
+r = requests.get('https://drive.google.com'+soup.select('#uc-download-link')[0]['href']) 
 with open("models.zip", "wb") as code:
       code.write(r.content)
 unzip_single('models.zip', 'models/')
