ab0313935d354cfc2db57a476c572dbee5d4271e
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-21 00:01:21 +0800

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,1 +1,8 @@
-import helpers_
+from helpers_and_configurations import *
+
+def rule1(input_coords, D1=D1):
+  
+    warning_coords = []
+    for input_coord in input_coords:
+        if input_coord['z'] < D1:
+        warning_coords
