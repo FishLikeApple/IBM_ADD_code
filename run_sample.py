61a5bee4f9718c86f9a292fb294b847d647463c8
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-20 10:00:16 +0800

    Update run_sample.py

--- run_sample.py
+++ run_sample.py
@@ -2,7 +2,7 @@ from helpers import *
 import requests 
 
 # unzip and load models
-unzip_single('models/models.zip', 'models/')
+unzip_single('IBM_ADD_code/models/models.zip', 'models/')
 import Model1, Model2, Model3, Model4
 
 # get input of all the models
