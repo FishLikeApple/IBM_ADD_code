5734473ed3486c42dcf1476950d6bca0dd914be2
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-22 17:27:37 +0800

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,40 +1,53 @@
 from helpers_and_configurations import *
 
-def rule1(input_coords, warning_coords=[], D1=D1, judgment_width=3):
+def rule1(input_coords, warning_coords=[], D=D1, judgment_width=3):
   
     for input_coord in input_coords:
         if input_coord not in warning_coords:
-            if input_coord['z']<D1 and np.abs(input_coord['x'])<judgment_width:
+            if input_coord['z']<D and np.abs(input_coord['x'])<judgment_width:
                 warning_coords.append(input_coord)
           
     return warning_coords
 
-def rule2(input_coords, warning_coords=[], D2=D2, pitch_threshold=0.03):
+def rule2(input_coords, warning_coords=[], D=D2, pitch_threshold=0.03):
   
     for input_coord in input_coords:
         if input_coord not in warning_coords:
-            if input_coord['z'] < D2:
+            if input_coord['z'] < D:
                 if input_coord['x'] > 0:
                     pitch = input_coord['pitch'] - np.pi/2
                 else:
                     pitch = input_coord['pitch'] + np.pi/2
-                if np.abs(np.arctan(input_coord['y']/input_coord['x'])-pitch) > pitch_threshold:
+                if np.abs(np.arctan(input_coord['y']/input_coord['x'])-pitch) < pitch_threshold:
                     warning_coords.append(input_coord)
           
     return warning_coords
   
-def rule3(input_coords, D2=D2, pitch_threshold=0.03):
+def rule3(input_coords, warning_coords=[], D=D3, pitch_threshold=0.03):
   
     warning_coords = []
     for input_coord in input_coords:
-        if input_coord['z'] < D1:
-          pitch = input_coord['pitch'] + np.pi/2
-#          if np.abs(pitch)<np.pi/2 and input_coord[]:
+        if input_coord['z'] < D:
+            if input_coord['x'] > 0:
+                    pitch = -input_coord['pitch']
+                else:
+                    pitch = input_coord['pitch']
+                if pitch<180+pitch_threshold and pitch>0-pitch_threshold:
+                    warning_coords.append(input_coord)
+                    
+    return warning_coords
 
-def rule4(input_coords, D2=D2, pitch_threshold=0.03):
+def rule4(input_coords, warning_coords=[], D=D4, pitch_threshold=0.03):
+    # actually the same as the above func
   
     warning_coords = []
     for input_coord in input_coords:
-        if input_coord['z'] < D1:
-          pitch = input_coord['pitch'] + np.pi/2
-#          if np.abs(pitch)<np.pi/2 and input_coord[]:
+        if input_coord['z'] < D:
+            if input_coord['x'] > 0:
+                    pitch = -input_coord['pitch'] - np.pi/2
+                else:
+                    pitch = input_coord['pitch']
+                if pitch<180+pitch_threshold and pitch>0-pitch_threshold:
+                    warning_coords.append(input_coord)
+                   
+    return warning_coords
