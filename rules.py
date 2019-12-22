7f9aa107668afeff9f3dee5bc191c54265a75844
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-22 11:48:48 +0800

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,8 +1,18 @@
 from helpers_and_configurations import *
 
-def rule1(input_coords, D1=D1):
+def rule1(input_coords, D1=D1, car_width=):
+  
+    warning_coords = []
+    for input_coord in input_coords:
+        if input_coord['z']<D1 and np.aps(input_coord['x'])<car_width*1.1:
+          warning_coords.append(input_coord)
+          
+    return warning_coords
+
+def rule2(input_coords, D2=D2):
   
     warning_coords = []
     for input_coord in input_coords:
         if input_coord['z'] < D1:
-        warning_coords
+          pitch = input_coord['pitch'] + np.pi/2
+          if np.abs(pitch)<np.pi/2 and input_coord[]:
