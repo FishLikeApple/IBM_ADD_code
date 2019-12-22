f606a65d6c3bc164cfedbfa8612c95b41eec9ef5
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-22 15:25:54 +0800

    Update rules.py

--- rules.py
+++ rules.py
@@ -4,7 +4,7 @@ def rule1(input_coords, D1=D1, car_width=6):
   
     warning_coords = []
     for input_coord in input_coords:
-        if input_coord['z']<D1 and np.aps(input_coord['x'])<car_width*1.1:
+        if input_coord['z']<D1 and np.abs(input_coord['x'])<car_width*1.1:
           warning_coords.append(input_coord)
           
     return warning_coords
